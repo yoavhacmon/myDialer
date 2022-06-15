7aeb4bd8666ed5b603686a5bfda9cfb416dcdd69
yoavhacmon <yoavh1234@gmail.com>
2022-06-15 14:44:07 +0300

    my dialer

---
+++ app/src/main/java/com/yoav/mydialer/MainActivity.java
@@ -0,0 +1,84 @@
+package com.yoav.mydialer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView phoneNumber;
+    private RelativeLayout num1;
+    private boolean isDailing = false;
+    private ImageView dialerImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setPointer();
+    }
+
+    public void setPointer(){
+        phoneNumber = findViewById(R.id.sum);
+        dialerImage = findViewById(R.id.dialerImage);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.num1:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("1"));
+                break;
+            case R.id.num2:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("2"));
+                break;
+            case R.id.num3:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("3"));
+                break;
+            case R.id.num4:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("4"));
+                break;
+            case R.id.num5:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("5"));
+                break;
+            case R.id.num6:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("6"));
+                break;
+            case R.id.num7:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("7"));
+                break;
+            case R.id.num8:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("8"));
+                break;
+            case R.id.num9:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("9"));
+                break;
+            case R.id.num0:
+                phoneNumber.setText(phoneNumber.getText().toString().concat("0"));
+                break;
+            case R.id.clr:
+                phoneNumber.setText(
+                        phoneNumber.getText().toString()
+                                .substring(0, phoneNumber.getText().length() - 1)
+                );
+                break;
+            case R.id.dial:
+                isDailing=!isDailing;
+                if (isDailing){
+                    Toast.makeText(this, "dialing to "+ phoneNumber.getText().toString(), Toast.LENGTH_LONG).show();
+                    dialerImage.setImageResource(R.drawable.hangup);
+                }
+                else {
+                    dialerImage.setImageResource(R.drawable.dial);
+                    phoneNumber.setText("");
+                }
+                break;
+        }
+    }
+}
